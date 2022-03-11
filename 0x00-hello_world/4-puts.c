3ed1bbd6cf780b384f61e91438aa2d9e1a811a9e
nohalyu <redilyu@gmail.com>
2022-03-11 11:33:20 +0300

    My 4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/*main -prints the phrase "Programming is like building a multilingual puzzle*/
-/*Return: 0 if exited properly, non-zero otherwise*/
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
