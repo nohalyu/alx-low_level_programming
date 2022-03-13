af2623c2cadb53b66858614e3d237d44a54a8639
nohalyu <redilyu@gmail.com>
2022-03-13 12:26:28 +0300

    My first commit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,12 +19,12 @@ if (l > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 	else
-if (l == 0)
+if (l < 6)
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 	else
-if (Lap < 6)
+if (Lap == 6)
 {
 	printf("Last digit of %d is %d and is 0 and is 0\n", n, l);
 }
