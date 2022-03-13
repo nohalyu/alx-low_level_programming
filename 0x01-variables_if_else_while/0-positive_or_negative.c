6ec475e78b9df116ee8667ce17859b6973dbb682
nohalyu <redilyu@gmail.com>
2022-03-13 10:55:39 +0300

    My zero task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-/* main-Entry point */
-/* Return always 0 (success) */
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
 int n;
