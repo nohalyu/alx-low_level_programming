8987850dec02059b7db97612c9267b99ecbf3bb3
nohalyu <redilyu@gmail.com>
2022-03-16 15:40:16 +0300

    My zero task on functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-
-
 /**
  * main - Entry point
  *
- * Return: nothing, return void
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char *sh = "_putchar";
 
-return (0);
+	while (*sh)
+	{
+		putchar(*sh);
+		sh++;
+	}
+	putchar('\n');
+
+	return (0);
 }
